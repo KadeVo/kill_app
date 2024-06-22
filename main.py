7c1c32130bab01349833638fb73fe5c5a84ef99d
KadeVo <135204015+KadeVo@users.noreply.github.com>
2024-06-22 22:09:17 +1200

    Created app

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-if __name__ == "__main__":
-    print("Hello, World!")
+import psutil
+import time
+import os
+import signal
+
+
+
+def monitor_applications():
+  monitor_applications = ['ProtonVPN.exe']
+  maxtime = 7200
+  for x in psutil.process_iter(attrs=['pid', 'name', 'create_time']):
+    try:
+      if x.info['name'] in monitor_applications:
+        runtime = time.time - x.info['create_time']
+      
+        if runtime >= maxtime:
+          os.kill(x.info['pid'], signal.SIGKILL)
+          print(f"Killed {x.info['name']} because it was alive for {runtime}")
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+      pass
+    
+monitor_applications()
